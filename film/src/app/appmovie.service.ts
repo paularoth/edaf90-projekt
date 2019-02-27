6a39c7ccac649173e7ea23ae901c79fae47abe2c
Anne Line Gjersem <annelinegj@gmail.com>
2019-02-27 11:30:12 +0100

    added movies.ts

---
+++ film/src/app/appmovie.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { appmovie } from './appmovie';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class movieService {
+    private movieURL = 'http://www.omdbapi.com/?t=Titanic';
+
+    constructor() {
+
+    }
+
+    getMovies(): Observable<[]
+    } 
