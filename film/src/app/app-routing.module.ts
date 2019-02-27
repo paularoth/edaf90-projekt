a573af7c29edad2ef7f3631f032ff064975bc38f
Anne Line Gjersem <annelinegj@gmail.com>
2019-02-27 10:51:10 +0100

    new route

--- film/src/app/app-routing.module.ts
+++ film/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ViewComponent } from './view/view.component';
 import { MovieComponent } from './movie/movie.component';
+import { HistoryComponent } from './history/history.component';
 
 const routes: Routes = [
   { path: 'view', component: HomepageComponent },
   { path: 'homepage', component: ViewComponent },
-  { path: 'movie', component: MovieComponent }
+  { path: 'movie', component: MovieComponent },
+  { path: 'history', component: HistoryComponent }
+
 ];
 
 @NgModule({
