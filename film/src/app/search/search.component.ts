7996bfca2f363f93c7ffaccd6a6ac41e92170f39
paularoth <dic15pro@student.lu.se>
2019-02-27 11:54:50 +0100

    fixes

--- film/src/app/search/search.component.ts
+++ film/src/app/search/search.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+import { Subject }    from 'rxjs/Subject';
+
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
+import { appmovie } from '../appmovie';
+import { movieService } from '../appmovie.service';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
+  movies$: Observable<appmovie[]>;
+  private searchTerms = new Subject<string>();
+  
+  constructor(private movieService: movieService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+  
+  ngOnInit(): void {
+    this.movies$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.movieService.searchHeroes(term))
+    );
   }
 
 }
