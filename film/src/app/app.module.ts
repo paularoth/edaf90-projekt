a0c27003ebebba19b265fd173233d132793c919c
Anne Line Gjersem <annelinegj@gmail.com>
2019-02-27 10:45:50 +0100

    add new components

--- film/src/app/app.module.ts
+++ film/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { ViewComponent } from './view/view.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MovieComponent } from './movie/movie.component';
+import { SearchComponent } from './search/search.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewComponent,
     HomepageComponent,
-    MovieComponent
+    MovieComponent,
+    SearchComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
