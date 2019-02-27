3e3d92aef360e7960c07093750461c9b1dec59ca
Anne Line Gjersem <annelinegj@gmail.com>
2019-02-27 09:56:15 +0100

    added components

--- film/src/app/app-routing.module.ts
+++ film/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ViewComponent } from './view/view.component';
+import { MovieComponent } from './movie/movie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'view', component: HomepageComponent },
+  { path: 'homepage', component: ViewComponent },
+  { path: 'movie', component: MovieComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- film/src/app/app.module.ts
+++ film/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewComponent } from './view/view.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    HomepageComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
