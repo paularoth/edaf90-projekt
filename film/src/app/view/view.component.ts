a190239dce73818c6c4f57ed59dfa126a328a3e8
Anne Line Gjersem <annelinegj@gmail.com>
2019-03-01 20:29:09 +0100

    showing poster different style3

--- film/src/app/view/view.component.ts
+++ film/src/app/view/view.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { movieService } from '../appmovie.service';
-import { catchError, map, tap, filter, toArray, take } from 'rxjs/operators';
-import { Observable, Subscription } from 'rxjs';
 import { MessageService } from '../message.service';
 
 @Component({
@@ -11,34 +9,25 @@ import { MessageService } from '../message.service';
   styleUrls: ['./view.component.scss']
 })
 export class ViewComponent implements OnInit {
-  private id: any;
   list;
-  private sub: any;
-  subscription: Subscription;
   private movies: Array<string> = [];
-
   constructor(private movieService: movieService, private route: ActivatedRoute,
     private messageService: MessageService) { }
 
   ngOnInit() {
-
     this.list = this.messageService.get();
-
     this.list = this.viewUnique();
-
     this.list.map(id => this.movieService.getMovies(id).subscribe(value => this.movies.push(value.Poster)))
-
   }
   viewUnique() {
     return this.list.filter(this.findUnique);
-
   }
-
   findUnique(value, index, self) {
     return self.indexOf(value) === index;
   }
 
-  removeMovie() {
+  removeMovie(id) {
+    this.list.filter(elem => elem == id);
 
   }
 }
